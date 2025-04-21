802ea5fd0649acfa1f2ad8569420091dd53670ba
shafiqDev <shafiqswat02@gmail.com>
2025-04-21 17:22:00 +0500

    change the Footer file name to footer

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -13,7 +13,7 @@ import { ChatProvider } from "@/context/chat.context";
 import { usePathname } from "next/navigation";
 import { LikeProvider } from "@/context/likes.context";
 import SidebarWrapper from "./sidebarWrapper/SidebarWrapper";
-import Footer from "@/components/layout/footer/Footer";
+import Footer from "./footer/Footer";
 
 const LayoutContent = ({ children }) => {
   const { isAuthLoading, isAuthenticated, user } = useAuth();
